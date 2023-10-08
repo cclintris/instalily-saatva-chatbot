6bec8ce8f548024512dc5b5f10584f852f19a1ca
cclintris <181250083@smail.nju.edu.cn>
2023-10-08 00:07:22 -0700

    add: completed general features of Saatva bot

--- server/app.py
+++ server/app.py
@@ -2,22 +2,16 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 from flask import make_response
 from utils import *
-import matplotlib
-
-matplotlib.use('Agg')
 
 app = Flask(__name__)
 CORS(app, resources={r"/*": {"origins": "*"}})
 app.config['CORS_HEADERS'] = 'Content-Type'
-
+            
 
 @app.route('/chat', methods=['POST'])
 def chat():
     question = request.json['question']
-    
-    df = build_embeddings_index()
-    response_text = chatbot_response(df, question)
-    print('[app.py response_text]: ', response_text)
+    response_text = ask(question)
     response = make_response(jsonify({'response': response_text}), 200)
     response.headers["Access-Control-Allow-Origin"] = "*"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
